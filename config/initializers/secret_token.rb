16e0cd6cbcf6601c58f754025f69fb33e0add37a
Luiz Branco <artfikan@gmail.com>
2012-02-22 21:29:57 -0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Puree::Application.config.secret_token = '6ef527f6e34e1d8c55a4f1f2469b06a0826d886c5d1ff72c30c5342cab9fb236462aceb8aaceeb2cf51157406087ba2eaeedad4caf1a260487126c4ddf4a87ff'
